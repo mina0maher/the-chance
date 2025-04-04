f6a6a49df3d912c9bcd3652a6b76347446e4dea9
mina0maher <mahermena160@gmail.com>
2025-04-04 14:27:57 +0200

    sudoku

--- sudoku/Check.kt
+++ sudoku/Check.kt
@@ -101,7 +101,7 @@ fun main() {
                 listOf('4', '-', '-', '8', '-', '3', '-', '-', '1'),
                 listOf('7', '-', '-', '-', '2', '-', '-', '-', '6'),
                 listOf('-', '6', '-', '-', '-', '-', '2', '8', '-'),
-                listOf('-', '-', '-', '4', '1', '9', '9', '-', '5'), // 2 nines in the subgrid
+                listOf('-', '-', '-', '4', '1', '7', '9', '-', '5'), // 2 nines in the subgrid
                 listOf('-', '-', '-', '-', '8', '-', '-', '7', '9')
             )
         ),
@@ -117,6 +117,24 @@ fun main() {
     validateSudoku(
         name = "when Sudoku is not a 2D nested list return false",
         result = isSudokuValid(listOf('1', '2', '3')),
+        correctResult = false
+    )
+
+    validateSudoku(
+        name = "when Sudoku is completely filled with valid numbers but integers return false",
+        result = isSudokuValid(
+            listOf(
+                listOf(5,3,4,6,7,8,9,1,2),
+                listOf(6,7,2,1,9,5,3,4,8),
+                listOf(1,9,8,3,4,2,5,6,7),
+                listOf(8,5,9,7,6,1,4,2,3),
+                listOf(4,2,6,8,5,3,7,9,1),
+                listOf(7,1,3,9,2,4,8,5,6),
+                listOf(9,6,1,5,3,7,2,8,4),
+                listOf(2,8,7,4,1,9,6,3,5),
+                listOf(3,4,5,2,8,6,1,7,9)
+            )
+        ),
         correctResult = false
     )
 
@@ -137,6 +155,8 @@ fun main() {
         ),
         correctResult = false
     )
+
+
 
     validateSudoku(
         name = "when Sudoku rows doesn't equals columns return false",
